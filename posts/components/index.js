bb44a3b13054963a215feee616bdf2c948c3c988
Philipp Wruck <philipp@perspective.co>
2017-10-16 11:42:33 +0200

    Adds index to posts components

--- posts/components/index.js
+++ posts/components/index.js
@@ -1,13 +1,31 @@
-import { List, ListItem } from './bullets-list'
-import { FootNotes, Ref, Note } from './footnote'
-import Figure from './figure'
-import Image from './image'
-import Video from './video'
-import { H2, H3, H4 } from './heading'
-import Code from './code'
-import P from './paragraph'
-import HR from './hr'
-import Meta from './meta'
+import { List, ListItem } from './bullets-list';
+import { FootNotes, Ref, Note } from './footnote';
+import Figure from './figure';
+import Image from './image';
+import Video from './video';
+import { H2, H3, H4 } from './heading';
+import Code from './code';
+import P from './paragraph';
+import HR from './hr';
+import Meta from './meta';
+
+export default {
+  List,
+  ListItem,
+  Code,
+  Figure,
+  Image,
+  Video,
+  P,
+  FootNotes,
+  Ref,
+  Note,
+  H2,
+  H3,
+  H4,
+  HR,
+  Meta,
+};
 
 export {
   List,
@@ -24,5 +42,5 @@ export {
   H3,
   H4,
   HR,
-  Meta
-}
+  Meta,
+};
