754dc6449468bb61522acab76b0374ead03c1307
Philipp Wruck <philipp@perspective.co>
2017-11-01 09:56:15 +0100

    Seperates HorizontalList and HorizontalListItem

--- src/modules/stats/components/horizontal-list/index.js
+++ src/modules/stats/components/horizontal-list/index.js
@@ -104,40 +104,4 @@ const HorizontalList = ( { region, children }: HorizontalListProps ) => (
 
 HorizontalList.defaultProps = { children : [] };
 
-type HorizontalListItemProps = {
-  metric: string,
-  value: string,
-};
-
-const HorizontalListItem = ( { metric, value }: HorizontalListItemProps ) => (
-  <div className="metric">
-    <div className="label">{metric.toUpperCase()}</div>
-    <div className="value">{value}</div>
-    <style jsx>{`
-      .metric {
-        display: inline-block;
-        width: 25%;
-        padding-bottom: 15px;
-      }
-
-      .label {
-        color: #50e3c2;
-        font-size: 12px;
-        margin-bottom: 5px;
-      }
-
-      .value {
-        font-size: 24px;
-      }
-
-      @media screen and (max-width: 700px) {
-        .metric {
-          padding-bottom: 20px;
-          width: 33%;
-        }
-      }
-    `}</style>
-  </div>
-);
-
-export { HorizontalList, HorizontalListItem };
+export default HorizontalList;
