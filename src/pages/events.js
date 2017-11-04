ef1cf58da3b1c99978553df674029ffc875b1ce0
Philipp Wruck <philipp@perspective.co>
2017-11-04 13:00:00 +0100

    Adds events page

---
+++ src/pages/events.js
@@ -0,0 +1,16 @@
+// @flow
+
+import * as React from 'react';
+
+import { Main, Head } from 'UI/Layout';
+
+const Index = () => {
+  return (
+    <Main>
+      <Head title="druphiny.co" />
+      <p>Placeholder</p>
+    </Main>
+  );
+};
+
+export default Index;
